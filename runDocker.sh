8236643e4b95a912d48ced08eb98c44158f2d791
Juan Milano <juan_milano@hotmail.com>
2016-05-20 18:41:00 -0400

    RunDocker sh

--- runDocker.sh
+++ runDocker.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+DOCKERFOLDER=/var/www/html
+LOCALFOLDER/var/www/html
+docker run -dP --name apachephp5fpm -v $LOCALFOLDER:$DOCKERFOLDER -p 80:80 apache_php5_fpm-gzip
 
-sudo docker run -d -p 80:80 chenweishi/nginx-php-fpm-varnish
 
