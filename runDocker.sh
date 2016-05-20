1e3d806351a5ae0a4ffdc56de70dde6fef3b7fad
Juan Milano <juan_milano@hotmail.com>
2016-05-20 18:42:40 -0400

    docker custom Volumen

--- runDocker.sh
+++ runDocker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 DOCKERFOLDER=/var/www/html
-LOCALFOLDER/var/www/html
+LOCALFOLDER=/var/www/html
 docker run -dP --name apachephp5fpm -v $LOCALFOLDER:$DOCKERFOLDER -p 80:80 apache_php5_fpm-gzip
 
 
